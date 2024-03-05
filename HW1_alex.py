23edb9b9724140c4230522bb28133f8ff435c876
ingeloflame <149815620+ingeloflame@users.noreply.github.com>
2024-03-05 19:00:23 +0500

    Add files via upload

---
+++ HW1_alex.py
@@ -0,0 +1,6 @@
+from transformers import pipeline
+
+
+translator = pipeline("translation_ru_to_en", "Helsinki-NLP/opus-mt-ru-en")
+translator("очень много материала надо изучить")
+print(translator)
