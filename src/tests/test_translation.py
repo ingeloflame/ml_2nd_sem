02bdc76042250f6656e7473abf878bc74a64baa5
ingeloflame <149815620+ingeloflame@users.noreply.github.com>
2024-03-06 13:51:05 +0500

    Update test_translation.py

--- src/tests/test_translation.py
+++ src/tests/test_translation.py
@@ -3,11 +3,11 @@ from transformers import pipeline
 
 def test_translation_pipeline():
     translator = pipeline("translation_ru_to_en", "Helsinki-NLP/opus-mt-ru-en")
-    
+
     input_text = "очень много материала надо изучить"
     expected_output = "There's a lot of material to study."
     result = translator(input_text)[0]["translation_text"]
-    
+
     assert (
-        result == expected_output
+            result == expected_output
     ), f"Expected '{expected_output}', but got '{result}'"
